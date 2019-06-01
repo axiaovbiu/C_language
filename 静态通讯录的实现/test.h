95eb2e47d4d86560878768e8b36ed056e3e7d46c
axiaovbiu.github.io <www.1127618534@qq.com>
2019-06-01 14:04:27 +0800

    c语言实现静态通讯录，功能包括： 1.添加联系人 2.删除联系人 3.查找联系人 4.修改联系人 5.展示通讯录 6.清除通讯录 7.联系人排序

---
+++ 静态通讯录的实现/test.h
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#ifndef _TEST_H_
+#define _TEST_H_
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<assert.h>
+#define NAME_SIZE 64
+#define TEL_SIZE 15
+#define ADDRE_SIZE 128
+#define MAX_SIZE 5
+typedef struct person
+{
+	char name[NAME_SIZE];
+	char sex;
+	unsigned char age;
+	char telphone[TEL_SIZE];
+	char address[ADDRE_SIZE];
+}person_t, *person_p, **person_pp;
+typedef struct contact
+{
+	int size;
+	person_t list[MAX_SIZE];
+}contact_t,*contact_p,**contact_pp;
+void Menu();
+void Init_Cont(contact_p cp);
+void Add_Person(contact_p cp);
+void Dele_Person(contact_p cp);
+void Search_Person(contact_p cp);
+void Mod_Person(contact_p cp);
+void Show_Person(contact_p cp);
+void Clear_Contact(contact_p cp);
+void Sort_Person(contact_p cp);
+
+#endif
